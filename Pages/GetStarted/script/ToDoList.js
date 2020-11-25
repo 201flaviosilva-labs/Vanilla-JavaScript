5ceecf227bbea4259d9bdf5801225e2bd460fdd1
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-11-25 19:20:34 +0000

    Update ToDoList.js

--- Pages/GetStarted/script/ToDoList.js
+++ Pages/GetStarted/script/ToDoList.js
@@ -2,13 +2,19 @@ const input = document.getElementById("InputToDoList");
 const submit = document.getElementById("SubmitToDoList");
 const list = document.getElementById("ListToDoList");
 
+createToDo("Start Learn JavaScript");
+
 submit.addEventListener("click", event => {
 	event.preventDefault();
+	createToDo(input.value);
+});
+
+function createToDo(todo) {
 	const li = document.createElement("li");
 	const button = document.createElement("button");
-	li.innerHTML = input.value;
+	li.innerHTML = todo;
 	input.value = "";
 	button.addEventListener("click", () => list.removeChild(li));
 	li.appendChild(button);
 	list.appendChild(li);
-});
+}
